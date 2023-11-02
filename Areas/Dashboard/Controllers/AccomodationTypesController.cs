643e8af265d019886f9cd5953a4d7eadfe2a92e8
Ban Alex <alexbanut10@gmail.com>
2023-11-02 19:30:11 +0200

    Accomodation Types dashboard index entry and create action

---
+++ Areas/Dashboard/Controllers/AccomodationTypesController.cs
@@ -0,0 +1,43 @@
+﻿using Check_Inn.Areas.Dashboard.ViewModels;
+using Check_Inn.Entities;
+using Check_Inn.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Check_Inn.Areas.Dashboard.Controllers
+{
+    public class AccomodationTypesController : Controller
+    {
+        AccomodationTypesService accomodationTypesService;
+
+        public AccomodationTypesController()
+        {
+            accomodationTypesService = new AccomodationTypesService();
+        }
+
+        // GET: Dashboard/AccomodationTypes
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult Listing()
+        {
+            AccomodationTypesListingModel model = new AccomodationTypesListingModel();
+
+            model.AccomodationTypes = accomodationTypesService.GetAllAccomodationTypes();
+
+            return PartialView("_Listing", model);
+        }
+
+        public ActionResult Action()
+        {
+            AccomodationTypeActionModel model = new AccomodationTypeActionModel();
+
+            return PartialView("_Action", model);
+        }
+    }
+}
---
+++ Services/AccomodationTypesService.cs
@@ -0,0 +1,19 @@
+﻿using Check_Inn.DAL;
+using Check_Inn.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Check_Inn.Services
+{
+    public class AccomodationTypesService
+    {
+        public IEnumerable<AccomodationType> GetAllAccomodationTypes()
+        {
+            CheckInnContext context = new CheckInnContext();
+
+            return context.AccomodationTypes.ToList();
+        }
+    }
+}

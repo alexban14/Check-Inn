755d1f7d5e9fffc43d5a437ec698f210f72f6fdd
Ban Alex <alexbanut10@gmail.com>
2023-11-12 15:22:54 +0200

    service layer for the Accomodation entity

---
+++ Areas/Dashboard/Controllers/AccomodationsController.cs
@@ -0,0 +1,94 @@
+﻿using Check_Inn.Entities;
+using Check_Inn.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Check_Inn.Areas.Dashboard.Controllers
+{
+    public class AccomodationsController : Controller
+    {
+        AccomodationsService accomodationsService;
+        AccomodationPackagesService accomodationPackagesService;
+
+        public AccomodationsController()
+        {
+            accomodationsService = new AccomodationsService();
+            accomodationPackagesService = new AccomodationPackagesService();
+        }
+
+        // GET: Dashboard/Accomodations
+        public ActionResult Index(string searchTerm, int? AccomodationPackageID, int? page)
+        {
+            return View();
+        }
+
+        // GET: Dashboard/Accomodations/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Dashboard/Accomodations/Create
+        [HttpPost]
+        public ActionResult Create(Accomodation model)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Dashboard/Accomodations/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Dashboard/Accomodations/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Accomodation model)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Dashboard/Accomodations/Delete/5
+        public ActionResult Delete(int ID)
+        {
+            return View();
+        }
+
+        // POST: Dashboard/Accomodations/Delete/5
+        [HttpPost]
+        public ActionResult Delete(Accomodation model)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Services/AccomodationsService.cs
@@ -0,0 +1,99 @@
+﻿using Check_Inn.DAL;
+using Check_Inn.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Check_Inn.Services
+{
+    public class AccomodationsService
+    {
+        public IEnumerable<Accomodation> GetAllAcomodation()
+        {
+            CheckInnContext context = new CheckInnContext();
+
+            return context.Accomodations.ToList();
+        }
+
+        public IEnumerable<Accomodation> SearchAccomodation(string searchTerm, int? AccomodationPackageID, int? page, int? recordSize)
+        {
+            CheckInnContext context = new CheckInnContext();
+
+            IEnumerable<Accomodation> accomodations = context.Accomodations.AsQueryable();
+
+            if(!string.IsNullOrEmpty(searchTerm))
+            {
+                accomodations = accomodations.Where(a => a.Name.ToLower().Contains(searchTerm.ToLower()) );
+            }
+
+            if (AccomodationPackageID.HasValue && AccomodationPackageID > 0)
+            {
+                accomodations = accomodations.Where(a => a.AccomodationPackageID == AccomodationPackageID.Value);
+            }
+
+            var skip = (page - 1) * recordSize;
+
+            return accomodations
+                .OrderBy(x => x.AccomodationPackageID)
+                .Skip((int)skip)
+                .Take((int)recordSize)
+                .ToList();
+        }
+
+        public int SearchAccomodationCount(string searchTerm, int? AccomodationPackageID)
+        {
+            CheckInnContext context = new CheckInnContext();
+
+            IEnumerable<Accomodation> accomodations = context.Accomodations.AsQueryable();
+
+            if(!string.IsNullOrEmpty(searchTerm))
+            {
+                accomodations = accomodations.Where(a => a.Name.ToLower().Contains(searchTerm.ToLower()) );
+            }
+
+            if (AccomodationPackageID.HasValue && AccomodationPackageID > 0)
+            {
+                accomodations = accomodations.Where(a => a.AccomodationPackageID == AccomodationPackageID.Value);
+            }
+
+            return accomodations.Count();
+        }
+
+        public Accomodation GetAccomodationPackageByID(int ID)
+        {
+            using (CheckInnContext context = new CheckInnContext())
+            { 
+                return context.Accomodations.Find(ID);
+            }
+        }
+
+        public bool SaveAccomodation(Accomodation accomodation)
+        {
+            CheckInnContext context = new CheckInnContext();
+
+            context.Accomodations.Add(accomodation);
+
+            return context.SaveChanges() > 0;
+        }
+
+        public bool UpdateAccomodation(Accomodation accomodation)
+        {
+            CheckInnContext context = new CheckInnContext();
+
+            context.Entry(accomodation).State = System.Data.Entity.EntityState.Modified;
+
+            return context.SaveChanges() > 0;
+        }
+
+        public bool DeleteAccomodation(Accomodation accomodation)
+        {
+            CheckInnContext context = new CheckInnContext();
+
+            context.Entry(accomodation).State = System.Data.Entity.EntityState.Deleted;
+
+            return context.SaveChanges() > 0;
+        }
+
+    }
+}
